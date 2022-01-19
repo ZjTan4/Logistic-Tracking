dbd466df7c6ea6eb2fd6e72975243d11d1cfa2b9
ZjTan4 <ZTAN4@UALBERTA.CA>
2022-01-18 16:18:23 -0800

    Work in Progress

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
+# Work in Progress
+
+First testable version is Expected to complete at 1/21/2022
+
 ### Requirements
 
 ```
-django
+Django
 uuid
 
 ```
--- backend/inventory/models.py
+++ backend/inventory/models.py
@@ -1,12 +1,14 @@
+from cProfile import label
 from pyexpat import model
+from unicodedata import category
 from django.db import models
 import uuid
 
 # Create your models here.
-class Inventory(models.Model):
+class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     
     name = models.CharField(max_length=50, default="unknown")
     description = models.TextField(max_length=200, default="")
 
-    
+    label = models.CharField(max_length=50, default="uncategorized")
